05e24969f8d038af959e2ea94444f4f549b819d7
Simon <81278617+simonsharayha@users.noreply.github.com>
2026-01-30 00:11:42 -0600

    clean urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Home } from './pages/Home'
 import { BusinessDetails } from './pages/BusinessDetails'
