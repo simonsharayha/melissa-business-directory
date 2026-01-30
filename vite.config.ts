5ec39ac05e6dcbdd7915b269284ecf4edf49744d
Simon <81278617+simonsharayha@users.noreply.github.com>
2026-01-29 22:58:38 -0600

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/melissa-business-directory/', // <-- repo name, with leading & trailing slash
 })
